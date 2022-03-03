73b385cabe3d923dd818ec16a65a481d5459a97d
Ty <ty.rickers@gmail.com>
2022-03-03 13:34:21 -0700

    made routes for testing auth middleware routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,45 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser } = require('../../utils/auth')
+const { User } = require('../../db/models')
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body});
 });
+
+
+router.get('/hello/world', function(req, res) {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.send('Hello World!');
+});
+
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-litionMan'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+      return res.json(req.user);
+    }
+  );
+
+  const { requireAuth } = require('../../utils/auth.js');
+  router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+      return res.json(req.user);
+    }
+  );
+
 
 module.exports = router;
