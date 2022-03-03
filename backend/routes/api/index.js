3b564740421d26ee3347578f2ad24df4b444ee0c
Ty <ty.rickers@gmail.com>
2022-03-03 13:34:49 -0700

    deleted the test routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -13,33 +13,5 @@ router.get('/hello/world', function(req, res) {
     res.send('Hello World!');
 });
 
-// GET /api/set-token-cookie
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-litionMan'
-      }
-    });
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
-
-router.get(
-    '/restore-user',
-    restoreUser,
-    (req, res) => {
-      return res.json(req.user);
-    }
-  );
-
-  const { requireAuth } = require('../../utils/auth.js');
-  router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-      return res.json(req.user);
-    }
-  );
-
 
 module.exports = router;
