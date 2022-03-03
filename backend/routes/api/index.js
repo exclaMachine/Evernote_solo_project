5fc29c7e58078b873d5ed62f10709278bd1c96f2
Ty <ty.rickers@gmail.com>
2022-03-03 13:56:34 -0700

    Created login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser } = require('../../utils/auth')
 const { User } = require('../../db/models')
+const sessionRouter = require('./session.js')
+const usersRouter = require('./users.js')
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body});
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const apiRouter = require('./api');
 
+
 router.use('/api', apiRouter);
 
 
