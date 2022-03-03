e130b027813c7298f026ee97fcf0758391d6b80f
Ty <ty.rickers@gmail.com>
2022-03-03 14:12:58 -0700

    created and tested logout route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -13,11 +13,4 @@ router.post('/test', function(req, res) {
     res.json({ requestBody: req.body});
 });
 
-
-router.get('/hello/world', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
-
-
 module.exports = router;
