08b3bdee064670752ec1e5e3ba30b46c860a27df
Ty <ty.rickers@gmail.com>
2022-03-05 05:51:29 -0700

    created models and migrated for notes

---
+++ backend/db/migrations/20220305123833-create-note.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Notes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      notebookId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Notes');
+  }
+};
---
+++ backend/db/models/note.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Note = sequelize.define('Note', {
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    notebookId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    content: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    }
+  }, {});
+  Note.associate = function(models) {
+    // associations can be defined here
+  };
+  return Note;
+};
