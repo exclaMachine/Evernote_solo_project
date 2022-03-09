7878fdbb5fc4866ef6d4aacfe67069ec63558a7a
Ty <ty.rickers@gmail.com>
2022-03-09 10:00:10 -0700

    added useSelector to provide session so added notebook has a userId

--- frontend/src/components/AddNotebook/index.js
+++ frontend/src/components/AddNotebook/index.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { postNotebookThunk } from "../../store/notebooks";
 
 const AddNotebook = () => {
     const dispatch = useDispatch();
+
+    const sessionUser = useSelector(state => state.session.user);
+
 
     const [title, setTitle] = useState('');
 
@@ -15,6 +18,7 @@ const AddNotebook = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const newNotebook = {
+            userId: sessionUser?.id,
             title
         }
 
